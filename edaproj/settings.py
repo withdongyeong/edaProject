b93660a0f5bc06987de282b87ff7c954f198b7b3
withdongyeong <withdongyeong@gmail.com>
2022-01-07 20:27:15 +0900

    update for validation page memory problem

--- edaproj/settings.py
+++ edaproj/settings.py
@@ -128,9 +128,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
-]
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

75cb54b84cfc2d2066f87cb8c66748950c90516a
withdongyeong <withdongyeong@gmail.com>
2022-01-07 18:14:19 +0900

    update gitignore and settings.py

--- edaproj/settings.py
+++ edaproj/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^#$e82e=0o*!2_np&$wqek5v$-gj(is+*qker3o(+y)1q0#qri'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-^#$e82e=0o*!2_np&$wqek5v$-gj(is+*qker3o(+y)1q0#qri')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 

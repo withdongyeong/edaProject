9f2834814b6b363b719dd52c5f274c17f9276532
withdongyeong <withdongyeong@gmail.com>
2022-01-07 22:06:16 +0900

    update statics

--- edaproj/settings.py
+++ edaproj/settings.py
@@ -142,3 +142,5 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 SITE_ID=1
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
